2bd4adbe74eee6aee98621a6c8322a426cb6383c
IsraelFelipe1997 <92111905+IsraelFelipe1997@users.noreply.github.com>
2025-03-24 16:57:44 -0300

    update sobre nos
    - modificado animação dos cards
    - adicionado animação no icone de whats

---
+++ Site/scripts/menu_scroll_change_color.js
@@ -0,0 +1,24 @@
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('#menu ul li a');
+
+function changeActiveLink() {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${current}`) {
+            link.classList.add('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', changeActiveLink);
